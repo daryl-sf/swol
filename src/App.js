31580441b57f996eb8a34b6e80f33e20ba17a93c
Daryl Findlay <darylfindlay@gmail.com>
2019-11-29 22:59:46 +0000

    add helemt, snapshot and router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Helmet title="You Are Doing Great!" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 	<p>Hello Daryl</p>
