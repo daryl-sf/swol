19ced568c35761faa3867371fe92fbd2a2943c9b
Daryl Findlay <darylfindlay@gmail.com>
2019-11-30 19:40:05 +0000

    add react router

--- src/App.js
+++ src/App.js
@@ -3,22 +3,11 @@ import Helmet from 'react-helmet';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <Helmet title="You Are Doing Great!" />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-	<p>Hello Daryl</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {props.children}
     </div>
   );
 }
