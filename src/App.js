8612402e926dd9eac0cff74d8077e47407cac209
Daryl Findlay <darylfindlay@gmail.com>
2019-12-01 22:47:35 +0000

    refactor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 import './App.css';
 
-function App(props) {
+function App() {
   return (
-    <div className="App">
-      <Helmet title="You Are Doing Great!" />
-      {props.children}
-    </div>
+    <Router>
+      <div className="App">
+        <Helmet title="You Are Doing Great!" />
+        <h2>Welcome to my site</h2>
+        <nav>
+          <ul>
+            <li>
+              <Link to={'/'}>Home</Link>
+            </li>
+            <li>
+              <Link to={'/about'}>About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,34 +1,7 @@
 import React from 'react';
 import { render } from 'react-snapshot';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Home from './Home';
-import About from './About';
 import './index.css';
 import App from './App';
 
-const app = (
-  <Router>
-    <div>
-      <h2>Welcome to my site</h2>
-      <nav>
-        <ul>
-          <li>
-            <Link to={'/'}>Home</Link>
-          </li>
-          <li>
-            <Link to={'/about'}>About</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-    <App>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-      </Switch>
-    </App>
-  </Router>
-);
-
-render(app, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
 
